03ce848835d965b04bffbf7057642b7e52286aa6
Markus Huber <markus.huber@tngtech.com>
2026-03-19 10:48:56 +0100

    add stuff

--- simulate_python/config.py
+++ simulate_python/config.py
@@ -1,14 +1,14 @@
-ROBOT = "go2" # Robot name, "go2", "b2", "b2w", "h1", "go2w", "g1" 
-ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene.xml" # Robot scene
-DOMAIN_ID = 1 # Domain id
-INTERFACE = "lo" # Interface 
+ROBOT = "g1"
+ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene_29dof.xml"
+DOMAIN_ID = 0
+INTERFACE = "wlp9s0" # ADD WLAN PORT INDIVIDUALLY
 
-USE_JOYSTICK = 1 # Simulate Unitree WirelessController using a gamepad
-JOYSTICK_TYPE = "xbox" # support "xbox" and "switch" gamepad layout
-JOYSTICK_DEVICE = 0 # Joystick number
+USE_JOYSTICK = 1
+JOYSTICK_TYPE = "xbox"
+JOYSTICK_DEVICE = 0
 
-PRINT_SCENE_INFORMATION = True # Print link, joint and sensors information of robot
-ENABLE_ELASTIC_BAND = False # Virtual spring band, used for lifting h1
+PRINT_SCENE_INFORMATION = True
+ENABLE_ELASTIC_BAND = True
 
-SIMULATE_DT = 0.005  # Need to be larger than the runtime of viewer.sync()
-VIEWER_DT = 0.02  # 50 fps for viewer
+SIMULATE_DT = 0.002
+VIEWER_DT = 0.02
